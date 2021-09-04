bd93bc50fdcbb219bada46df1ebd0edb289e8db2
shougo001 <kitazawa@samubuc.com>
2021-09-04 09:27:23 +0900

    Vueセットアップ

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,19 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+// import Rails from "@rails/ujs"
+// import Turbolinks from "turbolinks"
+// import * as ActiveStorage from "@rails/activestorage"
+// import "channels"
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+// Rails.start()
+// Turbolinks.start()
+// ActiveStorage.start()
+
+import Vue from 'vue'
+import App from './app.vue'
+
+const app = new Vue({
+   el: '#app',
+   render: h => h(App)
+})
